674e49cd417504c046c4d8d6e39e707d560fda24
liu zhenming <1119264845@qq.com>
2019-07-23 10:13:53 +0800

    Create a project

---
+++ upload-service/src/main/java/com/elfop/gofastdfs/uploadservice/UploadServiceApplication.java
@@ -0,0 +1,19 @@
+package com.elfop.gofastdfs.uploadservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Description:
+ * @author: liu zhenming
+ * @version: V1.0
+ * @date: 2019/7/23  10:11
+ */
+@SpringBootApplication
+public class UploadServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UploadServiceApplication.class, args);
+    }
+
+}
---
+++ webflux-upload-service/src/main/java/com/elfop/gofastdfs/webfluxuploadservice/WebfluxUploadServiceApplication.java
@@ -0,0 +1,19 @@
+package com.elfop.gofastdfs.webfluxuploadservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Description:
+ * @author: liu zhenming
+ * @version: V1.0
+ * @date: 2019/7/23  10:11
+ */
+@SpringBootApplication
+public class WebfluxUploadServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebfluxUploadServiceApplication.class, args);
+    }
+
+}
